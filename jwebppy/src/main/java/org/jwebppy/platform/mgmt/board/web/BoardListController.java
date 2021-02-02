8e551cd002a603aa0d1a37e0053c81cef8d4391d
Dalbong <Dalbong@DESKTOP-4AO8KK3>
2021-02-02 11:07:41 +0900

    플랫폼 리펙토링

--- jwebppy/src/main/java/org/jwebppy/platform/mgmt/board/web/BoardListController.java
+++ jwebppy/src/main/java/org/jwebppy/platform/mgmt/board/web/BoardListController.java
@@ -5,11 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/platform/mgmt/board")
-public class BoardController extends MgmtGeneralController
+@RequestMapping("/platform/mgmt/board/list")
+public class BoardListController extends MgmtGeneralController
 {
 	@RequestMapping("/main")
 	public String main()
+	{
+		return DEFAULT_VIEW_URL;
+	}
+
+	@RequestMapping("/write")
+	public String write()
 	{
 		return DEFAULT_VIEW_URL;
 	}
