ea590a838c5452b51e4335a892de1f874dbbf958
Dalbong <Dalbong@DESKTOP-4AO8KK3>
2021-03-10 22:01:34 +0900

    플랫폼 외 영역 삭제

--- jwebppy/src/main/java/org/jwebppy/portal/scm/domestic/claim/service/ClaimCreateService.java
+++
@@ -1,9 +0,0 @@
-package org.jwebppy.portal.scm.domestic.claim.service;
-
-import org.jwebppy.portal.PortalGeneralService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ClaimCreateService extends PortalGeneralService
-{
-}
