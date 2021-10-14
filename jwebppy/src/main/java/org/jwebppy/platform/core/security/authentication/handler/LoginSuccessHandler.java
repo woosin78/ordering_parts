3d751d6ff303881ae26c798beae8968dd97dcd55
i0830760 <i0830760@inyoungN2109.corp.doosan.com>
2021-10-14 17:55:00 +0900

    플랫폼 개발

--- jwebppy/src/main/java/org/jwebppy/platform/core/security/authentication/handler/LoginSuccessHandler.java
+++ jwebppy/src/main/java/org/jwebppy/platform/core/security/authentication/handler/LoginSuccessHandler.java
@@ -3,10 +3,13 @@ package org.jwebppy.platform.core.security.authentication.handler;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jwebppy.platform.core.PlatformCommonVo;
 import org.jwebppy.platform.core.security.authentication.service.LoginHistoryService;
+import org.jwebppy.platform.core.util.CmStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -25,6 +28,13 @@ public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessH
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException
 	{
 		loginHistoryService.success(request, response);
+
+		//Login 시 Save Username 체크 했을 경우 처리
+        if (CmStringUtils.equals(request.getParameter("saveUsername"), PlatformCommonVo.YES))
+        {
+        	Cookie cookie = new Cookie("username", authentication.getName());
+        	response.addCookie(cookie);
+        }
 
 		super.onAuthenticationSuccess(request, response, authentication);
 	}
