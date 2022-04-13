722208ba03901eef5caff83f86d031c39eccc23b
Dalbong <Dalbong@DESKTOP-4AO8KK3>
2022-04-13 22:56:22 +0900

    tomcat 설정

--- src/main/java/org/jwebppy/JwebppyApplication.java
+++ src/main/java/org/jwebppy/JwebppyApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableAspectJAutoProxy
-public class JwebppyApplication
+public class JwebppyApplication extends SpringBootServletInitializer
 {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+	{
+		return builder.sources(JwebppyApplication.class);
+	}
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(JwebppyApplication.class, args);
