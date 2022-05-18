700ba8add700d67c9b138b3f622c3db21abf4653
i0830760 <i0830760@inyoungN2109.corp.doosan.com>
2022-05-18 18:59:51 +0900

    어플리케이션 정보보안점검 대응

--- src/main/java/org/jwebppy/platform/core/exception/GlobalExceptionHandler.java
+++ src/main/java/org/jwebppy/platform/core/exception/GlobalExceptionHandler.java
@@ -3,6 +3,10 @@ package org.jwebppy.platform.core.exception;
 import javax.servlet.http.HttpServletRequest;
 
 import org.jwebppy.platform.core.PlatformConfigVo;
+import org.jwebppy.platform.core.util.CmStringUtils;
+import org.jwebppy.platform.core.web.ResponseMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.view.RedirectView;
@@ -10,17 +14,14 @@ import org.springframework.web.servlet.view.RedirectView;
 @ControllerAdvice
 public class GlobalExceptionHandler
 {
-//	@ExceptionHandler(Exception.class)
-//	public ResponseEntity<Map<String, Object>> handleException(HttpServletRequest request, Exception e)
-//	{
-//		e.printStackTrace();
-//
-//		return new ResponseEntity<>(ResponseMessage.toJson(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), ""), HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-
 	@ExceptionHandler(Exception.class)
-	public RedirectView handleException(HttpServletRequest request, Exception e)
+	public Object handleException(HttpServletRequest request, Exception e)
 	{
+        if (CmStringUtils.equals("XMLHttpRequest", request.getHeader("X-Requested-With")))
+        {
+        	return new ResponseEntity<>(ResponseMessage.toJson(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), ""), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
 		return new RedirectView(PlatformConfigVo.ERROR_PAGE_URL);
     }
 }
