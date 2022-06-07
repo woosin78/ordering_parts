110a98ec9b68e26c71168eeb4d2b06c75cbd691f
i0830760 <i0830760@inyoungN2109.corp.doosan.com>
2022-06-07 15:06:11 +0900

    시스뱅크 주문연동 페이지

--- src/main/java/org/jwebppy/portal/iv/hq/parts/domestic/order/create/web/OrderCreateByInterfaceController.java
+++ src/main/java/org/jwebppy/portal/iv/hq/parts/domestic/order/create/web/OrderCreateByInterfaceController.java
@@ -1,22 +1,19 @@
 package org.jwebppy.portal.iv.hq.parts.domestic.order.create.web;
 
-import org.jwebppy.portal.iv.hq.parts.common.PartsErpDataMap;
 import org.jwebppy.portal.iv.hq.parts.domestic.common.PartsDomesticCommonVo;
+import org.jwebppy.portal.iv.hq.parts.domestic.common.web.PartsDomesticGeneralController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
-@RequestMapping(PartsDomesticCommonVo.REQUEST_PATH + "/order/create")
-public class OrderCreateByInterfaceController extends OrderCreateController
+@RequestMapping(PartsDomesticCommonVo.REQUEST_PATH + "/order/create/gate")
+public class OrderCreateByInterfaceController extends PartsDomesticGeneralController
 {
-	@RequestMapping("/if/{source}")
-	public String source(@PathVariable String source, Model model, WebRequest webRequest)
+	@RequestMapping
+	public String source(Model model, WebRequest webRequest)
 	{
-		PartsErpDataMap rfcParamMap = getErpUserInfo();
-
 		addAllAttributeFromRequest(model, webRequest);
 
 		return DEFAULT_VIEW_URL;
