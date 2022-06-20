2f62793bf3a3b33d445a527b5e3ad8c397ec6fb8
i0830760 <i0830760@inyoungN2109.corp.doosan.com>
2022-06-20 15:27:29 +0900

    Error Msg = ORA-01722: invalid number 임시 조치

--- src/main/java/org/jwebppy/portal/iv/survey/service/SurveyService.java
+++ src/main/java/org/jwebppy/portal/iv/survey/service/SurveyService.java
@@ -27,66 +27,67 @@ public class SurveyService extends PlatformGeneralService
 {
 	@Autowired
 	private SurveyMapper surveyMapper;
-	
+
 	@Autowired
 	private SurveyTargetService surveyTargetService;
-	
+
 	@Autowired
 	private SurveyItemService surveyItemService;
 
 	@Autowired
 	private SurveyQuestionService surveyQuestionService;
-	
+
 	@Autowired
 	private EpUploadFileListService uploadFileListService;
-	
+
 	@Autowired
 	private EpUploadFileService uploadFileService;
-	
+
 	public final String UPLOAD_NAME = "SURVEY";
-	
-	public SurveyDto getSurvey(int sSeq) 
+
+	public SurveyDto getSurvey(int sSeq)
 	{
 		SurveyDto survey = new SurveyDto();
 		survey.setSSeq(sSeq);
-		
+
 		return getSurvey(survey);
 	}
-	
-	public SurveyDto getSurvey(SurveyDto survey) 
+
+	public SurveyDto getSurvey(SurveyDto survey)
 	{
 		return CmModelMapperUtils.mapToDto(SurveyObjectMapper.INSTANCE, surveyMapper.findSurvey(survey));
 	}
-	
-	public SurveyDto getLatestSurvey(SurveySearchDto surveySearchDto) 
+
+	public SurveyDto getLatestSurvey(SurveySearchDto surveySearchDto)
 	{
-		surveySearchDto.setUSeq(UserAuthenticationUtils.getUserDetails().getUSeq());
-		return CmModelMapperUtils.mapToDto(SurveyObjectMapper.INSTANCE, surveyMapper.findLatestSurvey(surveySearchDto));
+		//surveySearchDto.setUSeq(UserAuthenticationUtils.getUserDetails().getUSeq());
+		//return CmModelMapperUtils.mapToDto(SurveyObjectMapper.INSTANCE, surveyMapper.findLatestSurvey(surveySearchDto));
+		return null;
 	}
 
-	public List<SurveyDto> getSurveys(SurveySearchDto surveySearchDto) 
+	public List<SurveyDto> getSurveys(SurveySearchDto surveySearchDto)
 	{
 		surveySearchDto.setUSeq(UserAuthenticationUtils.getUserDetails().getUSeq());
 		return CmModelMapperUtils.mapToDto(SurveyObjectMapper.INSTANCE, surveyMapper.findSurveys(surveySearchDto));
 	}
-	
+
 	public int save(SurveyDto survey) throws IOException
 	{
 		if (survey.getSSeq() == 0) {
 			return create(survey);
-		} 
-		else 
+		}
+		else
 		{
 			return modify(survey);
 		}
 	}
-	
+
 	public int create(SurveyDto survey) throws IOException
 	{
 		SurveyEntity surveyEntity = CmModelMapperUtils.mapToEntity(SurveyObjectMapper.INSTANCE, survey);
 		surveyMapper.insert(surveyEntity);
 		int sSeq = surveyEntity.getSSeq();
-		
+
 		EpUploadFileDto uploadFile = uploadFileService.getUploadFileByName(UPLOAD_NAME);
 		if (ObjectUtils.isNotEmpty(uploadFile)) {
 			if (uploadFileListService.save(uploadFile, Integer.toString(sSeq), survey.getFiles()) > 0) {
@@ -94,23 +95,23 @@ public class SurveyService extends PlatformGeneralService
 				surveyMapper.updateUfSeq(surveyEntity);
 			}
 		}
-		
+
 		if (ObjectUtils.isNotEmpty(survey.getSurveyTargets())) {
 			for (SurveyTargetDto surveyTarget: survey.getSurveyTargets()) {
 				surveyTarget.setSSeq(sSeq);
 			}
 			surveyTargetService.save(survey.getSurveyTargets());
 		}
-		
+
 		return sSeq;
 	}
-	
+
 	public int modify(SurveyDto survey) throws IOException
 	{
 		SurveyEntity surveyEntity = CmModelMapperUtils.mapToEntity(SurveyObjectMapper.INSTANCE, survey);
 		surveyMapper.update(surveyEntity);
 		int sSeq = surveyEntity.getSSeq();
-		
+
 		EpUploadFileDto uploadFile = uploadFileService.getUploadFileByName(UPLOAD_NAME);
 		if (ObjectUtils.isNotEmpty(uploadFile)) {
 			if (uploadFileListService.save(uploadFile, Integer.toString(sSeq), survey.getFiles()) > 0) {
@@ -119,45 +120,45 @@ public class SurveyService extends PlatformGeneralService
 			}
 		}
 		uploadFileListService.delete(survey.getUflSeqs());
-		
+
 		surveyTargetService.save(survey);
-		
+
 		return sSeq;
 	}
-	
+
 	public int delete(SurveyDto survey) throws IOException
 	{
 		// to-do 삭제불가로직
-		
+
 		int sSeq = survey.getSSeq();
-		
+
 		// item
 		SurveyItemDto surveyItem = new SurveyItemDto();
 		surveyItem.setSSeq(sSeq);
 		surveyItemService.deleteBySseq(surveyItem);
-		
+
 		// question
 		SurveyQuestionDto surveyQuestion = new SurveyQuestionDto();
 		surveyQuestion.setSSeq(sSeq);
 		surveyQuestionService.deleteBySseq(surveyQuestion);
-		
+
 		// taraget
 		SurveyTargetDto surveyTarget = new SurveyTargetDto();
 		surveyTarget.setSSeq(sSeq);
 		surveyTargetService.delete(surveyTarget);
-		
+
 		// files
 		List<EpUploadFileListDto> fileList = uploadFileListService.getUploadFileLists(surveyMapper.findUfSeq(sSeq), Integer.toString(sSeq));
 		if (ObjectUtils.isNotEmpty(fileList)) {
 			for (EpUploadFileListDto file : fileList) {
-				uploadFileListService.delete(file.getUflSeq()); 
+				uploadFileListService.delete(file.getUflSeq());
 			}
 		}
-		
+
 		// survey
 		SurveyEntity surveyEntity = CmModelMapperUtils.mapToEntity(SurveyObjectMapper.INSTANCE, survey);
 		surveyMapper.delete(surveyEntity);
-		
+
 		return 1;
 	}
 }
