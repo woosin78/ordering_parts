fbdf974874228a4c49e38edd75d56f3a51fa6bd1
Dalbong <Dalbong@DESKTOP-4AO8KK3>
2022-06-26 22:56:09 +0900

    session 설정 적용

--- src/main/java/org/jwebppy/config/SessionListener.java
+++ src/main/java/org/jwebppy/config/SessionListener.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SessionListener implements HttpSessionListener
 {
 	@Value("${server.servlet.session.timeout}")
