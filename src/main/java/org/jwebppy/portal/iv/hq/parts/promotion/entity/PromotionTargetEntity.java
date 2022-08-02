3ef6748922115163234d0d82972539242a3e134d
i0830760 <i0830760@inyoungN2109.corp.doosan.com>
2022-08-02 14:16:07 +0900

    고도화 프로모션&장바구니 개발

---
+++ src/main/java/org/jwebppy/portal/iv/hq/parts/promotion/entity/PromotionTargetEntity.java
@@ -0,0 +1,15 @@
+package org.jwebppy.portal.iv.hq.parts.promotion.entity;
+
+import org.jwebppy.portal.iv.hq.parts.common.entity.PartsGeneralEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class PromotionTargetEntity extends PartsGeneralEntity
+{
+	private static final long serialVersionUID = 7501998799786585300L;
+}
